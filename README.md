770e3e960a25fcb1bfadfb14b7816aad408a3ce0
invzhi <invzhi@protonmail.com>
2017-10-16 20:46:37 +0800

    Add insertion sort

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # sorting-visualization
 
-Sorting visualization inspired by [FishyMcFishFace](https://imgur.com/t/rainbow/RM3wl) on [Hacker News](https://news.ycombinator.com/item?id=15423202).
+Sorting visualization is inspired by [FishyMcFishFace](https://imgur.com/t/rainbow/RM3wl) on [Hacker News](https://news.ycombinator.com/item?id=15423202).
 
-![Selection Sort](gifs/image.gif)
+## Selection Sort
+
+![Selection Sort GIF](gifs/selection.gif)
+
+## Insertion Sort
+
+![Insertion Sort GIF](gifs/insertion.gif)
--- sort/insertion.go
+++ sort/insertion.go
@@ -1,5 +1,34 @@
 package sort
 
-func Insertion(a []uint8, y int, g *gif.GIF) {
-	
+import (
+	"image/gif"
+	"github.com/invzhi/sorting-visualization/gif256"
+)
+
+func InsertionSort(a []uint8, y int, g *gif.GIF) {
+	frame := 1
+	l := len(a)
+
+	e := 0
+	for i := l - 1; i > 0; i-- {
+		if a[i] < a[i - 1] {
+			a[i], a[i - 1] = a[i - 1], a[i]
+			e++
+		}
+	}
+	if e == 0 {
+		return
+	}
+
+	var j int
+	for i := 1; i < l; i++ {
+		t := a[i]
+		for j = i; a[j - 1] > t; j-- {
+			a[j] = a[j - 1]
+		}
+		a[j] = t
+
+		gif256.SetLine(g, y, frame, a)
+		frame++
+	}
 }
