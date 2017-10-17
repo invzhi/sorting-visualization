d37a9c4b1f1c765548d24ce938cc95c05a4f160f
invzhi <invzhi@protonmail.com>
2017-10-17 19:03:45 +0800

    Add overview for shell sort

--- sort/shell.go
+++ sort/shell.go
@@ -28,3 +28,28 @@ func ShellSort(a []uint8, y int, g *gif.GIF) {
 		h /= 3
 	}
 }
+
+func ShellSortOverview(a []uint8, y int, g *gif.GIF) {
+	frame := 1
+	l := len(a)
+
+	h := 1
+	for h < l/3 {
+		h = 3*h + 1
+	}
+	for h >= 1 {
+		var j int
+		for i := h; i < l; i++ {
+			t := a[i]
+			for j = i; j >= h && a[j-h] > t; j -= h {
+				a[j] = a[j-h]
+			}
+			a[j] = t
+
+		}
+		h /= 3
+
+		gif256.SetLine(g, y, frame, a)
+		frame++
+	}
+}
