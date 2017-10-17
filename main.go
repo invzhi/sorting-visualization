84f46e8a3e7c8807591ebb75de9286b27416b7fe
invzhi <invzhi@protonmail.com>
2017-10-17 19:21:34 +0800

    Use function sortHue to generate each gif

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"runtime"
 	"github.com/invzhi/sorting-visualization/gif256"
 	"github.com/invzhi/sorting-visualization/sort"
+	"image/gif"
+	"runtime"
+	"sync"
 )
 
 const side = 256
 
-func main() {
+func sortHue(sortF func([]uint8, int, *gif.GIF), fn string, delay int) {
 	var wg sync.WaitGroup
 
 	numCPU := runtime.NumCPU()
@@ -18,18 +19,26 @@ func main() {
 
 	g, cis := gif256.NewRandGIF(side, side)
 
+	fmt.Print(fn, " (0-255): ")
 	wg.Add(side)
 	for y := 0; y < side; y++ {
 		sem <- struct{}{}
 		go func(y int) {
 			defer wg.Done()
-			defer func() {<-sem}()
-			sort.SelectionSort(cis[y], y, g)
+			defer func() { <-sem }()
+			sortF(cis[y], y, g)
 		}(y)
-		fmt.Print(y, " ")
+		fmt.Print(y, ", ")
 	}
 
 	wg.Wait()
-	gif256.EncodeGIF(g, "gifs/se.gif")
-	fmt.Println("Good")
+	gif256.EncodeGIF(g, fn, delay)
+	fmt.Println("OK!")
+}
+
+func main() {
+	sortHue(sort.SelectionSort, "gifs/selection.gif", 0)
+	sortHue(sort.InsertionSort, "gifs/insertion.gif", 0)
+	sortHue(sort.ShellSort, "gifs/shell.gif", 0)
+	sortHue(sort.ShellSortOverview, "gifs/shell_overview.gif", 100)
 }
