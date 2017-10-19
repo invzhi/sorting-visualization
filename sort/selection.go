ec1d7165238e73aaff7aaf74e9d05f97b9bb3e75
invzhi <invzhi@protonmail.com>
2017-10-19 19:58:25 +0800

    Fix details in selection sort

--- sort/selection.go
+++ sort/selection.go
@@ -9,9 +9,9 @@ func SelectionSort(a []uint8, y int, g *gif.GIF) {
 	frame := 1
 	l := len(a)
 
-	for i := 0; i < l; i++ {
+	for i := 0; i < l-1; i++ {
 		minIndex := i
-		for j := i; j < l; j++ {
+		for j := i + 1; j < l; j++ {
 			if a[j] < a[minIndex] {
 				minIndex = j
 			}
